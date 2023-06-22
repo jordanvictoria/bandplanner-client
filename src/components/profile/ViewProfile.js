94919497311c5f6c964aca0561add7d505a187f9
Jordan Victoria <jgomeskuehner@gmail.com>
2023-06-22 16:57:34 -0500

    added profile view css

--- src/components/profile/ViewProfile.js
+++ src/components/profile/ViewProfile.js
@@ -19,41 +19,69 @@ export const ViewProfile = () => {
 
 
 
+
     return <>
-        <div className="container">
-            <div className="profileBandName">
-                {bandUserObj.full_name}'s Profile
-            </div>
+        <div className="site-background hero is-fullheight">
+            <Link to={`/profile/${parseInt(bandUserObj.id)}/edit`}>
+                <button className="editProfile"
+                >Edit Profile</button>
+            </Link>
+
             <div className="profileContainer">
-                <div>
-                    <img src={bandUserObj.photo} alt="profile photo" />
-                </div>
-                <div>Artist: {bandUserObj.project_title}</div>
-                <div>Bio: {bandUserObj.bio}</div>
-                <div>Link to Streaming: 
-                <a href={bandUserObj.streaming} target="_blank" rel="noopener noreferrer"> {bandUserObj.streaming}</a>
-                </div>
-                <div>Website: 
-                <a href={bandUserObj.website} target="_blank" rel="noopener noreferrer"> {bandUserObj.website}</a>
+                <div className="profileContent">
+                    <div className="profileInfo">
+                        <div className="artistName"><b>Artist:</b> {bandUserObj.project_title}</div>
+                        <div className="artistBio"><b>Bio:</b> {bandUserObj.bio}</div>
                     </div>
-                <div>Instagram: 
-                <a href={bandUserObj.instagram} target="_blank" rel="noopener noreferrer"> {bandUserObj.instagram}</a>
+                    <div className="profilePhoto">
+                        <img src={bandUserObj.photo} alt="profile photo" />
+                    </div>
                 </div>
-                <div>Twitter: 
-                <a href={bandUserObj.twitter} target="_blank" rel="noopener noreferrer"> {bandUserObj.twitter}</a>
+                <div className="profileLinks">
+                    <div>
+                        {bandUserObj.streaming ? (
+                            <a href={bandUserObj.streaming} target="_blank" rel="noopener noreferrer"> Streaming</a>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div>
+                        {bandUserObj.website ? (
+                            <a href={bandUserObj.website} target="_blank" rel="noopener noreferrer"> Website</a>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div>
+                        {bandUserObj.instagram ? (
+                            <a href={bandUserObj.instagram} target="_blank" rel="noopener noreferrer"> Instagram</a>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div>
+                        {bandUserObj.twitter ? (
+                            <a href={bandUserObj.twitter} target="_blank" rel="noopener noreferrer"> Twitter</a>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div>
+                        {bandUserObj.facebook ? (
+                            <a href={bandUserObj.facebook} target="_blank" rel="noopener noreferrer"> Facebook</a>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div>
+                        {bandUserObj.tiktok ? (
+                            <a href={bandUserObj.tiktok} target="_blank" rel="noopener noreferrer"> Tiktok</a>
+                        ) : (
+                            ""
+                        )}
+                    </div>
                 </div>
-                <div>Facebook: 
-                <a href={bandUserObj.facebook} target="_blank" rel="noopener noreferrer"> {bandUserObj.facebook}</a>
-                </div>
-                <div>Tiktok: 
-                <a href={bandUserObj.tiktok} target="_blank" rel="noopener noreferrer"> {bandUserObj.tiktok}</a>
-                </div>
-                <div>
-                    <Link to={`/profile/${parseInt(bandUserObj.id)}/edit`}>
-                        <button className="editProfile"
-                        >Edit Profile</button>
-                    </Link>
-                </div>
+
             </div>
         </div>
     </>
